63255554ad1665158f8367c5962589eef22b6a29
Bagus135 <bagusmustaqim35@gmail.com>
2024-07-17 16:36:29 +0700

    coeg

--- Server/db.js
+++ Server/db.js
@@ -1,9 +1,9 @@
 const {Pool} = require('pg');
 require('dotenv').config();
 
-const pool  = new Pool ({
-    connectionString : process.env.SupaLink
-})
 
+const pool = new Pool({
+    connectionString: "postgres://default:bmDAX0N6exMj@ep-empty-haze-a13riik7-pooler.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require?sslmode=require",
+  })
 
 module.exports = pool;
