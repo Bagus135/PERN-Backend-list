94a89b3b4d65535885d17730285d4b6b0719ef97
Bagus135 <bagusmustaqim35@gmail.com>
2024-07-17 16:41:07 +0700

    aaa

--- Server/auth/authRoutes.js
+++ Server/auth/authRoutes.js
@@ -27,7 +27,7 @@ router.post("/register", validator, async (req,res)=>{
         const token = jwtGenerator(newUser.rows[0].id, "user");
         res.json({token});
     } catch (error) {
-        res.status(500).send("Internal Server Error")
+        res.status(500).send(error.message)
     }
 })
 
